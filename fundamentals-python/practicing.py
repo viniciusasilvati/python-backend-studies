14c14da88d6d5e3d0e61f561ca8a6142226f7007
Vinicius Silva <viniciusasilva.ti@gmail.com>
2026-05-28 14:48:20 -0300

    Add new features: implement number guessing game, task manager, and ATM functionality; refactor rock-paper-scissors game for better user feedback

--- fundamentals-python/practicing.py
+++ fundamentals-python/practicing.py
@@ -1,3 +1,4 @@
+print('*************************************************')
 print('Calculadora de gorjeta \n')
 
 valor_conta = float(input('Digite o valor da conta: '))
@@ -81,7 +82,7 @@ print(f'Senha gerada: {gerar_senha()}')
 
 print('*************************************************')
 
-# import random
+import random
 
 print('Pedra, Papel, Tesoura \n')
 
@@ -91,21 +92,180 @@ def pedra_papel_tesoura():
     escolha_usuario = input('Escolha: pedra, papel ou tesoura? ').lower()
     
     if escolha_usuario not in opcoes:
-        return 'Escolha inválida!'
+        print('Escolha inválida!')
     
     print(f'Computador escolheu: {escolha_computador}')
 
     if escolha_usuario == escolha_computador:
-        return 'Empate!'
+        print('Empate!')
     
     elif (
         (escolha_usuario == 'tesoura' and escolha_computador == 'pedra') or
         (escolha_usuario == 'papel' and escolha_computador == 'tesoura') or
         (escolha_usuario == 'pedra' and escolha_computador == 'papel')
     ):
-        return 'Você venceu!'
+        print('Você ganhou!')
     
     else:
-        return 'Você perdeu!'
+        print('Você perdeu!')
     
-print(pedra_papel_tesoura())
+pedra_papel_tesoura()
+
+print('*************************************************')
+
+import random
+
+print('Descubra o número secreto \n')
+
+def adivinhar_numero():
+    numero_secreto = random.randint(1,100)
+    tentativas = 0
+
+    while True:
+        try:
+            palpite = int(input('Digite um número entre 1 e 100: '))
+
+            if not 1 <= palpite <= 100:
+                raise ValueError('Número fora do intervalo! Digite um número entre 1 e 100.')
+
+            tentativas += 1
+
+            if palpite == numero_secreto:
+                print(f'Parabéns! Você acertou o número {numero_secreto}.')
+                break
+            elif palpite < numero_secreto:
+                print(f'Muito baixo! Tente novamente: {palpite}')
+            else:
+                print(f'Muito alto! Tente novamente: {palpite}')
+
+        except ValueError as e:
+            print(f'Entrada inválida: {e}')
+
+adivinhar_numero()
+
+print('*************************************************')
+
+print('Calculadora \n')
+
+def somar(num1, num2):
+    return num1 + num2
+
+def subtrair(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def dividir(num1, num2):
+    if num2 == 0:
+        raise ZeroDivisionError
+    return num1 / num2
+
+def calculadora():
+    try:
+        num1 = float(input('Digite o primeiro número: '))
+        operacao = input('Escolha a operação (+, -, *, /): ')
+        num2 = float(input('Digite o segundo número: '))
+
+        if operacao == '+':
+            resultado = somar(num1, num2)
+        elif operacao == '-':
+            resultado = subtrair(num1, num2)
+        elif operacao == '*':
+            resultado = multiplicar(num1, num2)
+        elif operacao == '/':
+            resultado = dividir(num1, num2)
+        else:
+            print('Operação inválida!')
+            return
+
+        print(f'Resultado: {resultado}')
+
+    except ValueError:
+        print('Erro: Entrada inválida. Digite apenas números.')
+    except ZeroDivisionError:
+        print('Erro: Divisão por zero não é permitida.')
+    
+calculadora()
+
+print('*************************************************')
+
+print('Gerenciador de tarefas \n')
+
+def gerenciador_tarefas():
+    tarefas = []
+
+    while True:
+        print('1: Adicionar uma tarefa')
+        print('2: Visualizar a lista detarefas')
+        print('3: Remover uma tarefa')
+        print('4: Sair do programa')
+        escolha = input('\nEscolha uma opção: ')
+
+        if escolha == '1':
+            tarefa = input('\nDigite a tarefa a ser adicionada: ').strip()
+            if tarefa:
+                tarefas.append(tarefa)
+                print('Tarefa adicionada com sucesso!\n')
+            else:
+                print('Erro: A tarefa não pode estar vazia!\n')
+
+        elif escolha == '2':
+            if tarefas:
+                print('\nLista de Tarefas:')
+                for i, tarefa in enumerate(tarefas, 1):
+                    print(f'{i}. {tarefa}')
+            else:
+                print('Nenhuma tarefa na lista.\n')
+
+        elif escolha == '3':
+            if not tarefas:
+                print('Nenhuma tarefa para remover.\n')
+                continue
+            try:
+                indice = int(input('\nDigite o número da tarefa a ser removida: ')) - 1
+                if 0 <= indice < len(tarefas):
+                    tarefa_removida = tarefas.pop(indice)
+                    print(f'Tarefa "{tarefa_removida}" removida com sucesso!\n')
+                else:
+                    print('Erro: Número da tarefa inválido!\n')
+            except ValueError:
+                print('Erro: Entrada inválida. Digite um número válido!\n')
+
+        elif escolha == '4':
+            print('\nSaindo do programa. Até mais!')
+            break
+        else:
+            print('Opção inválida! Por favor, escolha uma opção válida.\n')
+
+gerenciador_tarefas()
+
+print('*************************************************')
+
+print('Caixa eletrônico \n')
+
+def caixa_eletronico():
+    cedulas = [100, 50, 20, 10, 5, 2]
+    
+
+    try:
+        valor_saque = int(input('Digite o valor a ser sacado: '))
+
+        if valor_saque <= 0:
+            print('Erro: O valor deve ser maior que zero!')
+            
+        elif valor_saque % 2 != 0:
+            print('Erro: O valor deve ser múltiplo de 2!')
+            
+        else:
+            print('\nCédulas entregues:')
+            for cedula in cedulas:
+                quantidade = valor_saque // cedula
+                if quantidade > 0:
+                    print(f'{quantidade} cédula(s) de R$ {cedula}')
+                    valor_saque = valor_saque % cedula
+
+    except ValueError:
+        print('Erro: Entrada inválida. Digite um número inteiro válido!')
+
+caixa_eletronico()
